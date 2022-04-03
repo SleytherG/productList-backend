07a24c2399f73995bcc62634a769650399d7bf1a
SleytherG <sleyther.calsin@cloudcomputing.com.pe>
2022-04-03 16:12:02 -0500

    make it better

--- src/main/java/com/productsapp/products/management/ProductsManagementApplication.java
+++ src/main/java/com/productsapp/products/management/ProductsManagementApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ProductsManagementApplication {
@@ -14,14 +13,17 @@ public class ProductsManagementApplication {
 		SpringApplication.run(ProductsManagementApplication.class, args);
 	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurerAdapter() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:5000");
-//			}
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedHeaders("*")
+						.allowedMethods("*");
+			}
+		};
+	}
 
 }
